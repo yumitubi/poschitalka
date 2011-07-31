0a5544bf48d044f96cc5af95afc6bf326c30f334
Mak Tom <starsman87@mail.ru>
2011-07-31 23:30:31 +0600

    разбор как работает окно открытия файла

--- openfile.py
+++ openfile.py
@@ -8,13 +8,13 @@ import gtk, sys
 
 def whatnamefile():
     namefile = openfile.get_filename()    
-    return namefile
+    poschitalka.txtbuf.set_text(namefile)
 
     #диалог открытия файла
 def on_clk_open(poschitalka):
     openfile = gtk.FileSelection("Открыть файл")
     openfile.run()
-    itog = openfile.ok_button.connect("clicked", whatnamefile)
+    openfile.ok_button.connect("clicked", whatnamefile)
     openfile.destroy()
-    poschitalka.wins.set_buffer(itog)
+    
 
