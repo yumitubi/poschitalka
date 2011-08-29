4d7fc7c15e7111efd7a528cecd64db06e8ff0382
Mak Tom <starsman87@mail.ru>
2011-08-29 22:51:25 +0600

    продолжение разборок с текстовым буфером

--- tests/textbuffer.py
+++ tests/textbuffer.py
@@ -12,10 +12,6 @@ import gtk, sys
 import os
 import string
 
-#подключаем дополнительные модули
-import aboutpunkt 
-import openfile
-
 
 #определяем глобальные переменные
 #текстовый буфер
@@ -40,69 +36,21 @@ class poschitalka(gtk.Window):
 #            sys.exit(1)
 
 
-        self.set_title("Посчиталка")#обзываем заголовок
+        self.set_title("test с текстовым буфером")#обзываем заголовок
         self.set_size_request(650,550)#размеры окна
         self.set_position(gtk.WIN_POS_CENTER)#ставим окно по центру
         self.connect("destroy", gtk.main_quit)#обработчик кнопки на закрытие окна
-
-
-        #############################################
-        #--------------рисуем меню------------------#
-        menub = gtk.MenuBar()
-        #меню файл
-        filemenu = gtk.Menu()
-        filem = gtk.MenuItem("Файл")
-        filem.set_submenu(filemenu)
-        
-        #пункт меню открыть
-        openf = gtk.MenuItem("Открыть файл")
-        openf.connect("activate", openfile.on_clk_open)
-        filemenu.append(openf)
-        #пункт меню открыть
-        savestat = gtk.MenuItem("Сохранить статистику")
-        filemenu.append(savestat)
-        #пункт меню выход
-        close = gtk.MenuItem("Выход")
-        close.connect("activate", gtk.main_quit)#по нажатии закрываем программу
-        filemenu.append(close)#добавляем в меню пункт выход
-        #меню справка 
-        helpmenu = gtk.Menu()
-        helper = gtk.MenuItem("Справка")
-        helper.set_submenu(helpmenu)
-        #пункт меню справка
-        helperitem = gtk.MenuItem("Справка")
-        helpmenu.append(helperitem)
-
-
-        #пункт меню о программе
-        #создаем объект класса aboutm
-        #aboutpunkt = aboutm()
-        about = gtk.MenuItem("О программе")
-        helpmenu.append(about)
-        about.connect("activate", aboutpunkt.on_clk_about)#диалог О программе
-
-        #добавляем созданные меню в меню бар
-        menub.append(filem)
-        menub.append(helper)
-
-
 
 
         ##################################################
         #-------------------кнопки----------------------#
         btn_close = gtk.Button("Выход")#рисуем кнопку выход, тем не менее, для нее еще нужен будет обработчик
         btn_close.connect("clicked", gtk.main_quit)#обрабатываем клик на кнопку, как закрытие окна
-        btn_close.set_tooltip_text("Нажмите, чтобы выйти из программы")
-        btn_close.set_size_request(65, 30)
-        #сохранить статистику
-        btn_save_stat = gtk.Button("Сохранить статистику")
-        btn_save_stat.set_tooltip_text("Нажмите, что бы сохранить статистику по тексту в файл")
-        btn_save_stat.set_size_request(190, 30)
+
         
         #тестовая кнопка
         btn_test = gtk.Button("Изменить значение буфера")
-        btn_test.connect("clicked", self.replacetb())
-
+        btn_test.connect("clicked", self.replacetb)
         #создаем область для прокручивания
         scroolwin = gtk.ScrolledWindow()
         scroolwin.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
@@ -122,32 +70,12 @@ class poschitalka(gtk.Window):
         #прописываем окно просмотра текста в прокручиваемой области
         scroolwin.add(wins)
 
-
-        
-        #вывод статистики
-        titlestat = gtk.Label("Статистика текста:")
-        
-        #статус бар
-        statusbar = gtk.Statusbar()
-        statusbar.push(1, "Ready")
-        
-        #выравнивание
-        halign = gtk.Alignment(0, 0, 0, 0)
-        halign.add(titlestat)
-        menualign = gtk.Alignment(0, 0, 0, 0)
-        menualign.add(menub)
-        menualign
-
-
         #таблица для разметки содержимого
         table = gtk.Table(10, 8, True)
         table.set_row_spacings(4)#устанавливаем расстояния между строчками таблицы
         table.set_col_spacings(4)#устанавливаем расстояния между столбцами таблицы
-        table.attach(menualign, 0, 8, 0, 1, gtk.FILL, gtk.FILL, 1, 1)
         table.attach(btn_close, 7, 8, 9, 10, gtk.EXPAND, gtk.EXPAND, 1, 1)#добавляем кнопку close
         table.attach(scroolwin, 0, 5, 1, 9, gtk.FILL, gtk.FILL, 1, 1)#добавляем текстовое поле
-        table.attach(halign, 5, 8, 1, 2, gtk.FILL, gtk.FILL, 0, 0)#добавляем выравнивание в таблице
-        table.attach(btn_save_stat, 4, 7, 9, 10, gtk.EXPAND, gtk.EXPAND, 1, 1)#добавляем кнопку сохранить статистику
         table.attach(btn_test, 3, 4, 9, 10, gtk.EXPAND, gtk.EXPAND, 1, 1)#добавляем кнопку test
 
 
@@ -165,7 +93,7 @@ class poschitalka(gtk.Window):
         self.add(table)#добавляем table в окно
         self.show_all()#даем команду все показать
 
-    def replacetb(btn_test):
+    def replacetb():
         txtbuf.set_text('получилось')
 
 #выполняем то, что определили функциями выше
